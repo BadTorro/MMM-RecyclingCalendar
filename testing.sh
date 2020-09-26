5b943e1c83c6bffa44994eda00b9443939959e5c
Andreas Pfeifer <andreaspfeifer@andreass-macbook-pro.home>
2020-09-26 21:45:15 +0200

    added testing script

---
+++ testing.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Running script..." 
+cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/mounts/modules/MMM-RecyclingCalendar"
+git pull
+cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/run"
+docker-compose up -d
