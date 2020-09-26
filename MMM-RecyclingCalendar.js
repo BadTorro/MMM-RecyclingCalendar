923f9e676b1172db61aa397315f5f2643684ad12
Andreas Pfeifer <andreaspfeifer@andreass-macbook-pro.home>
2020-09-26 19:36:20 +0200

    udpates

--- MMM-RecyclingCalendar.js
+++ MMM-RecyclingCalendar.js
@@ -2,13 +2,24 @@ Module.register("MMM-RecyclingCalendar", {
     defaults: {
         foo: "I'm alive!"
       },
-    start: function () {},
-    getDom: function() {
+    start: function (){ // is executed when module is loaded successfully 
+        this.count = 0
+        var timer = setInterval(()=>{
+          this.updateDom()
+          this.count++
+        }, 1000)
+      },
+      getDom: function() {
         var element = document.createElement("div")
         element.className = "myContent"
-        element.innerHTML = "Hello, World!" + this.config.foo
+        element.innerHTML = "Hello, World! " + this.config.foo
+        var subElement = document.createElement("p")
+        subElement.innerHTML = "Count:" + this.count
+        subElement.id = "COUNT"
+        element.appendChild(subElement)
         return element
       },
+      
     notificationReceived: function() {},
     socketNotificationReceived: function() {},
   })
