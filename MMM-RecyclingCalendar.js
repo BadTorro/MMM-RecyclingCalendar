10f34db40c3a13b28eb7375a44ea76d1e4e8b390
FatTony123 <andreaspfeifer@andreass-macbook-pro.home>
2020-09-27 20:58:50 +0200

    .

--- MMM-RecyclingCalendar.js
+++ MMM-RecyclingCalendar.js
@@ -26,65 +26,65 @@ Module.register("MMM-RecyclingCalendar", {
         this.getRecyclingData();
       },
 
-      getRecyclingData: function(){
-        this.sendSocketNotification('CALENDAR_GET', this.config);
-      },
+  getRecyclingData: function(){
+    this.sendSocketNotification('CALENDAR_GET', this.config);
+  },
       
-      notificationReceived: function(notification, payload, sender) {
-        if (sender) {
-          Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
-        } else {
-          Log.log(this.name + " received a system notification: " + notification);
-        }
+  notificationReceived: function(notification, payload, sender) {
+    if (sender) {
+      Log.log(this.name + " received a module notification: " + notification + " from sender: " + sender.name);
+    } else {
+      Log.log(this.name + " received a system notification: " + notification);
+    }
 
-        
-        // switch(notification) {
-        //   case "DOM_OBJECTS_CREATED":
-        //     var timer = setInterval(()=>{
-        //       this.sendSocketNotification("MMM-RECYCLINGCALENDAR-CONFIG", this.count)
-        //       this.count++
-        //     }, 1000)
-        //     break
-        // }
-      },
+    
+    // switch(notification) {
+    //   case "DOM_OBJECTS_CREATED":
+    //     var timer = setInterval(()=>{
+    //       this.sendSocketNotification("MMM-RECYCLINGCALENDAR-CONFIG", this.count)
+    //       this.count++
+    //     }, 1000)
+    //     break
+    // }
+  },
 
-      socketNotificationReceived: function(notification, payload) {
-        Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
+  socketNotificationReceived: function(notification, payload) {
+    Log.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
 
-        if(notification == "CALENDAR_RESULT" && payload['result'].length > 0){
-          // Log.log(payload['result']);
-          // Log.log("Payload length: "+payload['result'].length);
-          this.calendarData = payload['result'];  
+    if(notification == "CALENDAR_RESULT" && payload['result'].length > 0){
+      // Log.log(payload['result']);
+      // Log.log("Payload length: "+payload['result'].length);
+      this.calendarData = payload['result'];  
 
-          this.updateDom(1000);
-        }
+      this.updateDom(1000);
+    }
 
-      },
+  },
 
-      getDom: function() {
-        var wrapper = document.createElement("div");
+  getDom: function() {
+    var wrapper = document.createElement("div");
 
-        for(var i = 0; i<this.calendarData.length; i++){
-          var entry = this.calendarData[i];
+    for(var i = 0; i<this.calendarData.length; i++){
+      var entry = this.calendarData[i];
 
-          var entriesContainer = document.createElement("div");
-          entriesContainer.classList.add("entries-container");
+      var entriesContainer = document.createElement("div");
+      entriesContainer.classList.add("entries-container");
 
-          // add date 
-          var dateEntry = document.createElement("span");
-          dateEntry.classList.add("entry-date");
-          dateEntry.innerHTML = entry['date'];
-          entriesContainer.appendChild(dateEntry);
+      // add date 
+      var dateEntry = document.createElement("span");
+      dateEntry.classList.add("entry-date");
+      dateEntry.innerHTML = entry['date'];
+      entriesContainer.appendChild(dateEntry);
 
-          // add type 
-          var typeEntry = document.createElement("span");
-          typeEntry.classList.add("entry-type");
-          typeEntry.innerHTML = entry['type'];
-          entriesContainer.appendChild(typeEntry);
+      // add type 
+      var typeEntry = document.createElement("span");
+      typeEntry.classList.add("entry-type");
+      typeEntry.innerHTML = entry['type'];
+      entriesContainer.appendChild(typeEntry);
 
-          wrapper.appendChild(entriesContainer);
-        }
+      wrapper.appendChild(entriesContainer);
+    }
 
-        return wrapper;
-      },
-  })
+    return wrapper;
+  },
+})
