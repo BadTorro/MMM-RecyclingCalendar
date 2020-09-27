4cc09c6af1676cd7b12b16609a03b12a4e3dfda0
FatTony123 <andreaspfeifer@andreass-macbook-pro.home>
2020-09-27 21:10:07 +0200

    .,

--- testing.sh
+++ testing.sh
@@ -3,7 +3,7 @@
 echo "Running script..." 
 cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/mounts/modules/MMM-RecyclingCalendar"
 # git checkout getCalendarWeekData
-# git checkout stylesheet
+git checkout master
 git pull
 cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/run"
 docker-compose up
