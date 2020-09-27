3da2089a04b32745b256db0a38ec5830fe9a572b
FatTony123 <andreaspfeifer@andreass-macbook-pro.home>
2020-09-27 21:13:35 +0200

    removed testing

--- testing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Running script..." 
-cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/mounts/modules/MMM-RecyclingCalendar"
-# git checkout getCalendarWeekData
-# git checkout dev
-git pull
-cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/run"
-docker-compose up
