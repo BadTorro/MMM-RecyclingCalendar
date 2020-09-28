22d7771c3b2e2da6bb7f16269ffe7390248c2492
FatTony123 <andreaspfeifer@andreass-macbook-pro.home>
2020-09-28 18:08:39 +0200

    .

--- MMM-RecyclingCalendar.js
+++ MMM-RecyclingCalendar.js
@@ -81,7 +81,7 @@ Module.register("MMM-RecyclingCalendar", {
     var svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
     svg.setAttributeNS(null, "class", "entry-icon " + type);
     var use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-    use.setAttributeNS("http://www.w3.org/1999/xlink", "href", this.file("icons/can.svg#") + type);
+    use.setAttributeNS("http://www.w3.org/1999/xlink", "href", this.file("icons/icon_sprite.svg#") + type);
     svg.appendChild(use);
     
     return(svg);
