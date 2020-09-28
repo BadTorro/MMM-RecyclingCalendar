a39768c48e591f5a56f19569cc42764978a46c58
FatTony123 <andreaspfeifer@andreass-macbook-pro.home>
2020-09-28 09:30:05 +0200

    ,

--- MMM-RecyclingCalendar.js
+++ MMM-RecyclingCalendar.js
@@ -75,7 +75,7 @@ Module.register("MMM-RecyclingCalendar", {
       var dateEntry = document.createElement("span");
       dateEntry.classList.add("entry-date");
       var date = entry['date'];
-      date = moment(date, 'YYYY-MM-DD').format('DD');
+      date = moment(date, 'YYYY-MM-DD').format('dddd');
       dateEntry.innerHTML = date;
       entriesContainer.appendChild(dateEntry);
 
