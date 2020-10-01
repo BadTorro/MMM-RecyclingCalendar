ac34cc256733d667ea645e49ffb2ebe358d56d43
FatTony123 <andreaspfeifer@andreass-macbook-pro.home>
2020-10-01 18:42:28 +0200

    .

--- node_helper.js
+++ node_helper.js
@@ -27,7 +27,7 @@ module.exports = NodeHelper.create({
     params.append('end', enddate);
     params.append('sort', payload.sort);
     if(payload.showType){
-      params.append('types', payload.showType);
+      // params.append('types', payload.showType);
     }
 
     url.search = params.toString();
