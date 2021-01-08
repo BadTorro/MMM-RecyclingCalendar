e442f89e8c2f5f1ce6af1ad8743413524cddc171
FatTony123 <FatTony123@users.noreply.github.com>
2021-01-08 16:51:39 +0100

    update

--- MMM-RecyclingCalendar.js
+++ MMM-RecyclingCalendar.js
@@ -17,8 +17,7 @@ Module.register("MMM-RecyclingCalendar", {
     showExplanation: false, 
     showColorIcons: false,
     limitEntries: "50", 
-    // pollFrequency: 10 * 60 * 1000, // every 10 minutes 
-    pollFrequency: 1
+    pollFrequency: 10 * 60 * 1000, // every 10 minutes 
   },
   
 
