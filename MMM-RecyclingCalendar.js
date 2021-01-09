7603ce2b35f074a3120460a39bff206c08ff86ae
FatTony123 <FatTony123@users.noreply.github.com>
2021-01-09 21:01:44 +0100

    bugfixing

--- MMM-RecyclingCalendar.js
+++ MMM-RecyclingCalendar.js
@@ -158,7 +158,7 @@ Module.register("MMM-RecyclingCalendar", {
     }
 
     // add update hint 
-    if(this.config.showUpdateHint && this.updateTime !== undefined){
+    if(this.config.showUpdateHint && this.pollTime !== undefined){
       var updateHint = document.createElement('div');
       updateHint.className = 'xsmall dimmed italic'; 
       updateHint.innerHTML = 'Updated at '+this.updateTime; 
